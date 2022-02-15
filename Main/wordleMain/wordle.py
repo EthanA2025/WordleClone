d34dc4777acc1b33aa94a67f1a70344207c9e75b
etan Memeignton <eja9979@g.rit.edu>
2022-02-15 14:13:26 -0500

    guess test correct and incorrect

--- Main/wordleMain/wordle.py
+++ Main/wordleMain/wordle.py
@@ -3,10 +3,12 @@
 A game that is a clone of the popular website 'wordle'
 '''
 import random
+
 '''
 Chooses a random secret word from a csv file
 '''
-def chooseSecret(filename):
+
+def choose_secret(filename):
 
     with open(filename) as file:
         word_bank = file.read().strip().split()
@@ -24,25 +26,35 @@ def printWelcome():
 '''
 Prompts the user to enter a guess for the word
 '''
-# def enterguess(secretWord):
+def enter_guess(guess, secret_word):
+    result = -1
+    if (guess == secret_word):
+        print("This is the correct word!")
+        print(f"\u001b[33m{secret_word}\u001b[0m")
+        return result
+    else:
+        result = 0
+        return result
+    
+    # stops game loop and the user wins if guess and secret are equal.
+    
+    # for i in range(0, len(guess)):
+    #     if (guess[i] == secret_word[i]):
+    #         print("here")
+    #     elif (guess[i] in secret_word[i]):
+    #         print("in word")
+    #     else:
+    #         print("not in word")
 
-#     guess = input("Enter guess: ")
-#     for i in range(0, len(guess)):
-#         if (guess[i] == secretWord[i]):
-#             print("here")
-#         elif (guess[i] in secretWord[i]):
-#             print("in word")
-#         else:
-#             print("not in word")
-
-#     return -1
+    # return result
 
 def main():
     tries = 6 # the user has 6 tries to get the word correct
     printWelcome()
-    secret = chooseSecret("words/words.txt")
+    secret = choose_secret("Main/words/words.txt")
     while (tries > 1):
-        # enterguess(secret)
+        # guess = input("enter guess: ")
+        # enter_guess(guess, secret)
         tries -= 1
     
 main()
--- Main/wordleMain/wordle_test.py
+++ Main/wordleMain/wordle_test.py
@@ -3,5 +3,18 @@ import random
 
 def test_get_secret_redly():
     random.seed(10)
-    secret = wordle.chooseSecret("words/words.txt")
+    secret = wordle.choose_secret("Main/words/words.txt")
     assert secret == "redly"
+
+def test_secret_correct():
+    random.seed(10)
+    secret = wordle.choose_secret("Main/words/words.txt")
+    result = wordle.enter_guess(secret, "redly")
+    assert result == -1
+
+
+def test_secret_incorrect():
+    random.seed(10)
+    secret = wordle.choose_secret("Main/words/words.txt")
+    result = wordle.enter_guess(secret, "aaaaaaaaaaaaaaaaaaaaaaaaaaaa")
+    assert result == 0
