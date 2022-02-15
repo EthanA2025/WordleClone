c751ce415bf78002f54e37da175eff8a9c493766
etan Memeignton <eja9979@g.rit.edu>
2022-02-15 18:42:00 -0500

    bounds checking added

--- src/wordle.py
+++ src/wordle.py
@@ -26,25 +26,43 @@ def printWelcome():
     print(['_', '_', '_', '_', '_'])
 
 '''
+checks for a valid word.
+'''
+def check_valid(filename, guessed_word):
+    with open(filename) as file:
+        word_bank = file.read().strip().split()
+    if guessed_word not in word_bank:
+        return False
+    return True
+
+'''
 Prompts the user to enter a guess for the word
 '''
 def enter_guess(guess, secret_word):
+    guess = guess.lower()
     correct = False
+    valid = check_valid("words/words.txt", guess)
+    if len(guess) > 5 or not valid:
+        print("invalid word!")
+        return correct
     if guess == secret_word:
         print("This is the correct word!")
         print(f"\u001b[32m{secret_word}\u001b[0m")
         correct = True
         return correct # stops game loop and the user wins if guess and secret are equal.
     else:
-        for i in range(0, len(guess)): # check if the letters are in the right spot or in the word wrong spot
-            if (guess[i] == secret_word[i]):
-                green = guess[i]
-                guess[i] = "\u001b[32m" + green + "\u001b[0m" # change to green
-            elif (guess[i] in secret_word[i]):
-                yellow = guess[i]
-                guess[i] = "\u001b[33m" + yellow + "\u001b[0m" # change to yellow
+        word = ""
+        for i in range(0, len(guess)):
+            if guess[i] == secret_word[i]:
+                green = "\u001b[32m" + guess[i] + "\u001b[0m"
+                word += green # change to green
+            elif guess[i] in secret_word: # multiple instances in word
+                yellow = "\u001b[33m" + guess[i] + "\u001b[0m" # change to yellow
+                word += yellow
+            else:
+                word += guess[i]
         
-        print("Your guess results: " + guess)    
+        print("Your guess results: " + word)    
         return correct
 
 def game():
@@ -53,11 +71,12 @@ def game():
 
     printWelcome()
     secret = choose_secret("words/words.txt")
-    print(secret)
+    # print(secret)
     while tries > 1 and correct == False:
         guess = input("\nenter guess: ")
         correct = enter_guess(guess, secret)
         tries -= 1
+    print("the word was: " + secret)
 
 def main():
     game()
